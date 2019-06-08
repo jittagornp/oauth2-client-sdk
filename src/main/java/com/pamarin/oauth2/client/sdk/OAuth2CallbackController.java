b0600149786fc42f46398d1ad1b8a8711057208d
jittagornp <jittagornp@gmail.com>
2019-06-08 15:22:50 +0700

    set AuthorizeSuccessUrl

--- src/main/java/com/pamarin/oauth2/client/sdk/OAuth2CallbackController.java
+++ src/main/java/com/pamarin/oauth2/client/sdk/OAuth2CallbackController.java
@@ -8,7 +8,9 @@ import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import static org.springframework.util.StringUtils.hasText;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -17,13 +19,22 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class OAuth2CallbackController {
-    
+
     @Autowired
     private HostUrlProvider hostUrlProvider;
-    
+
+    @Value("${oauth2.session-filter.authorize-success-url:#{null}}")
+    private String authorizeSuccessUrl;
+
     @GetMapping("/oauth/callback")
-    public void callback(HttpServletRequest httpReq, HttpServletResponse httpResp) throws IOException{
-        httpResp.sendRedirect(hostUrlProvider.provide());
+    public void callback(HttpServletRequest httpReq, HttpServletResponse httpResp) throws IOException {
+        httpResp.sendRedirect(getAuthorizeSuccessUrl());
     }
-    
+
+    private String getAuthorizeSuccessUrl() {
+        if (!hasText(authorizeSuccessUrl)) {
+            return hostUrlProvider.provide();
+        }
+        return authorizeSuccessUrl;
+    }
 }
